a730428265c3e5469ebd6fe8f0176e93fca663d6
NoKi <69469555+uedai13871@users.noreply.github.com>
2026-04-21 14:16:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -108,8 +108,8 @@ def solve_shift():
     obj_terms = []
     for e in all_emps:
         for d in range(1, num_days+1):
-            obj_terms.append(shifts[(e, d, OFF)] * -1)
-            obj_terms.append(shifts[(e, d, DAY)] * 40)
+            # obj_terms.append(shifts[(e, d, OFF)] * -1)
+            # obj_terms.append(shifts[(e, d, DAY)] * 40)
 
     model.Maximize(sum(obj_terms))
     solver = cp_model.CpSolver()
